a873f98ee39fc4451761903c628040123434c1c6
ngophuc29 <ngophuc2911@gmail.com>
2024-04-12 14:41:45 +0700

    a

---
+++ NodeJs-MongoDB-Server/config/dbConfig.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // localUrl: "mongodb://localhost:27017/revit",
+  localUrl: "mongodb+srv://ngophuc2911:phuc29112003@cluster0.buhheri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test",
+  cloudUrl: "mongodb+srv://ngophuc2911:phuc29112003@cluster0.buhheri.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0/test"
+};
---
+++ client/src/pages/Auth.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+async function getAccesstoken(){
+    return axios.get('http://localhost:8080/token')
+    .then((response) => {
+        return response.data;
+    })
+    .catch((err) => {
+        console.log(err);
+    })
+};
+
+const Client = {getAccesstoken};
+export default Client;
